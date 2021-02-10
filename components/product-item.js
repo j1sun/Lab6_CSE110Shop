7a8ebb36634521d5ac4e269ca510922fdd65cb21
Justin Sun <j1sun@ucsd.edu>
2021-02-10 14:11:08 -0800

    Created product-item element

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,120 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+
+    // li element
+    const wrapper = document.createElement('li');
+    wrapper.setAttribute('class', 'product');
+    // img element
+    const img = wrapper.appendChild(document.createElement('img'));
+    // title element
+    const title = wrapper.appendChild(document.createElement('p'));
+    title.setAttribute('class', 'title');
+    // price element
+    const price = wrapper.appendChild(document.createElement('p'));
+    price.setAttribute('class', 'price');
+    // button element
+    const button = wrapper.appendChild(document.createElement('button'));
+    button.textContent = 'Add to Cart';
+    button.addEventListener('click', this.handleClick);
+    // CSS styling
+    const style = document.createElement('style');
+    style.textContent = `
+    .price {
+      color: green;
+      font-size: 1.8em;
+      font-weight: bold;
+      margin: 0;
+    }
+
+    .product {
+      align-items: center;
+      background-color: white;
+      border-radius: 5px;
+      display: grid;
+      grid-template-areas: 
+      'image'
+      'title'
+      'price'
+      'add';
+      grid-template-rows: 67% 11% 11% 11%;
+      height: 450px;
+      filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+      margin: 0 30px 30px 0;
+      padding: 10px 20px;
+      width: 200px;
+    }
+
+    .product > button {
+      background-color: rgb(255, 208, 0);
+      border: none;
+      border-radius: 5px;
+      color: black;
+      justify-self: center;
+      max-height: 35px;
+      padding: 8px 20px;
+      transition: 0.1s ease all;
+    }
+
+    .product > button:hover {
+      background-color: rgb(255, 166, 0);
+      cursor: pointer;
+      transition: 0.1s ease all;
+    }
+
+    .product > img {
+      align-self: center;
+      justify-self: center;
+      width: 100%;
+    }
+
+    .title {
+      font-size: 1.1em;
+      margin: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+
+    .title:hover {
+      font-size: 1.1em;
+      margin: 0;
+      white-space: wrap;
+      overflow: auto;
+      text-overflow: unset;
+    }`;
+
+    this.shadowRoot.append(style, wrapper);
+  }
+
+  connectedCallback() {
+    updateAttributes(this);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    updateAttributes(this);
+  }
+}
+
+function updateAttributes(element) {
+
+  const shadow = element.shadowRoot;
+
+  // img element
+  const img = shadow.querySelector('img');
+  img.setAttribute('src', element.getAttribute('img'));
+  img.setAttribute('alt', element.getAttribute('data-title'));
+  img.setAttribute('width', 200);
+  // title element
+  const title = shadow.querySelector('.title');
+  title.textContent = element.getAttribute('data-title');
+  // price element
+  const price = shadow.querySelector('.price');
+  price.textContent = element.getAttribute('data-price');
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,34 @@
 // Script.js
 
+function populateStorage(localStorage) {
+  // Fetch from URL
+  fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => localStorage.setItem('products', JSON.stringify(data)))
+    .then(() => setShop(localStorage))
+}
+
+function setShop(localStorage) {
+  let products = JSON.parse(localStorage.getItem('products'));
+  for (product of products) {
+    let element = document.createElement('product-item');
+    element.setAttribute('id', 'product-' + product.id);
+    element.setAttribute('img', product.image);
+    element.setAttribute('data-title', product.title);
+    element.setAttribute('data-price', product.price);
+    document.querySelector('#product-list').appendChild(element);
+  }
+
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+
+  // Check for local storage
+  let localStorage = window.localStorage;
+
+  if (!localStorage.getItem('products')) {
+    populateStorage(localStorage);
+  } else {
+    setShop(localStorage);
+  }
 });
